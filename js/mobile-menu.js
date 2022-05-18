010bcb3ab88f7a6aaf3b445e99837adb988f3114
c3c1l1a <cesswangumukima@gmail.com>
2022-05-18 21:44:05 +0800

    Add mobile menu with ability to open and close

---
+++ js/mobile-menu.js
@@ -0,0 +1,15 @@
+let hamburgerMenu = document.querySelector('.js-humburger-menu-icon');
+hamburgerMenu.addEventListener('click', (event) => {
+	if ('content' in document.createElement('template')) {
+		const templateMobileMenu = document.querySelector('.js-template-mobile-menu');
+		const mobileMenu = templateMobileMenu.content.firstElementChild.cloneNode(true);
+		
+		const closeIcon = mobileMenu.querySelector('.close-icon');
+		closeIcon.addEventListener('click', (event) => {
+			templateMobileMenu.parentNode.removeChild(mobileMenu);
+		});
+
+		document.querySelector('body').appendChild(mobileMenu);
+	}
+});
+
