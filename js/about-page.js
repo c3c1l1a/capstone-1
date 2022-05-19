e94d99d48dad2cfb9cf3878c218d72791c308571
c3c1l1a <cesswangumukima@gmail.com>
2022-05-19 23:17:54 +0800

    Fix js liniting errors

--- js/about-page.js
+++ js/about-page.js
@@ -1,36 +1,32 @@
 window.addEventListener('load', () => {
-	if ('content' in document.createElement('template')) {
-		
-		const aboutPageLink = document.querySelector('.js-about-page-link');
-		aboutPageLink.addEventListener('click', () => {
-			const homePage = document.querySelector('.js-home-page');
-			if (homePage)
-				homePage.parentNode.removeChild(homePage);
+  if ('content' in document.createElement('template')) {
+    const aboutPageLink = document.querySelector('.js-about-page-link');
+    aboutPageLink.addEventListener('click', () => {
+      const homePage = document.querySelector('.js-home-page');
+      if (homePage) homePage.parentNode.removeChild(homePage);
 
-			const aboutPage = document.querySelector('.js-about-page');
-			if (aboutPage === null){
-				const templateAboutPage = document.querySelector('.js-template-about-page');
-				const aboutPage = templateAboutPage.content.firstElementChild.cloneNode(true);
-				
-				const mainTag = document.querySelector('main');
-				mainTag.appendChild(aboutPage);
-			}
-		});
+      const aboutPage = document.querySelector('.js-about-page');
+      if (aboutPage === null) {
+        const templateAboutPage = document.querySelector('.js-template-about-page');
+        const aboutPage = templateAboutPage.content.firstElementChild.cloneNode(true);
 
-		const homePagetLink = document.querySelector('.js-home-page-link');
-		homePagetLink.addEventListener( 'click',  ()=> {
-			const aboutPage = document.querySelector('.js-about-page');
-			if (aboutPage)
-				aboutPage.parentNode.removeChild(aboutPage);
+        const mainTag = document.querySelector('main');
+        mainTag.appendChild(aboutPage);
+      }
+    });
 
-			const homePage = document.querySelector('.js-home-page');
-			if (homePage === null){
-				const templateHomePage = document.querySelector('.js-template-home-page');
-				const aboutPage = templateHomePage.content.firstElementChild.cloneNode(true);
-				const mainTag = document.querySelector('main');
-				mainTag.appendChild(aboutPage);
-			}
+    const homePagetLink = document.querySelector('.js-home-page-link');
+    homePagetLink.addEventListener('click', () => {
+      const aboutPage = document.querySelector('.js-about-page');
+      if (aboutPage) aboutPage.parentNode.removeChild(aboutPage);
 
-		});
-	}
+      const homePage = document.querySelector('.js-home-page');
+      if (homePage === null) {
+        const templateHomePage = document.querySelector('.js-template-home-page');
+        const aboutPage = templateHomePage.content.firstElementChild.cloneNode(true);
+        const mainTag = document.querySelector('main');
+        mainTag.appendChild(aboutPage);
+      }
+    });
+  }
 });
--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,49 +1,44 @@
-let hamburgerMenu = document.querySelector('.js-humburger-menu-icon');
-hamburgerMenu.addEventListener('click', (event) => {
-	if ('content' in document.createElement('template')) {
-		const templateMobileMenu = document.querySelector('.js-template-mobile-menu');
-		const mobileMenu = templateMobileMenu.content.firstElementChild.cloneNode(true);
-		
-		const closeIcon = mobileMenu.querySelector('.close-icon');
-		closeIcon.addEventListener('click', (event) => {
-			mobileMenu.parentNode.removeChild(mobileMenu);
-		});
+const hamburgerMenu = document.querySelector('.js-humburger-menu-icon');
+hamburgerMenu.addEventListener('click', () => {
+  if ('content' in document.createElement('template')) {
+    const templateMobileMenu = document.querySelector('.js-template-mobile-menu');
+    const mobileMenu = templateMobileMenu.content.firstElementChild.cloneNode(true);
 
+    const closeIcon = mobileMenu.querySelector('.close-icon');
+    closeIcon.addEventListener('click', () => {
+      mobileMenu.parentNode.removeChild(mobileMenu);
+    });
 
-		const aboutPagetLink = mobileMenu.querySelector('.js-about-page-link');
-		aboutPagetLink.addEventListener('click', () => {
-			const homePage = document.querySelector('.js-home-page');
-			if (homePage)
-				homePage.parentNode.removeChild(homePage);
+    const aboutPagetLink = mobileMenu.querySelector('.js-about-page-link');
+    aboutPagetLink.addEventListener('click', () => {
+      const homePage = document.querySelector('.js-home-page');
+      if (homePage) homePage.parentNode.removeChild(homePage);
 
-			const aboutPage = document.querySelector('.js-about-page');
-			if (aboutPage === null){
-				const templateAboutPage = document.querySelector('.js-template-about-page');
-				const aboutPage = templateAboutPage.content.firstElementChild.cloneNode(true);
-				const mainTag = document.querySelector('main');
-				mainTag.appendChild(aboutPage);
-			}
-			mobileMenu.parentNode.removeChild(mobileMenu);
-			
-		});
+      const aboutPage = document.querySelector('.js-about-page');
+      if (aboutPage === null) {
+        const templateAboutPage = document.querySelector('.js-template-about-page');
+        const aboutPage = templateAboutPage.content.firstElementChild.cloneNode(true);
+        const mainTag = document.querySelector('main');
+        mainTag.appendChild(aboutPage);
+      }
+      mobileMenu.parentNode.removeChild(mobileMenu);
+    });
 
-		const homePagetLink = mobileMenu.querySelector('.js-home-page-link');
-		homePagetLink.addEventListener( 'click',  ()=> {
-			const aboutPage = document.querySelector('.js-about-page');
-			if (aboutPage)
-				aboutPage.parentNode.removeChild(aboutPage);
+    const homePagetLink = mobileMenu.querySelector('.js-home-page-link');
+    homePagetLink.addEventListener('click', () => {
+      const aboutPage = document.querySelector('.js-about-page');
+      if (aboutPage) aboutPage.parentNode.removeChild(aboutPage);
 
-			const homePage = document.querySelector('.js-home-page');
-			if (homePage === null){
-				const templateHomePage = document.querySelector('.js-template-home-page');
-				const aboutPage = templateHomePage.content.firstElementChild.cloneNode(true);
-				const mainTag = document.querySelector('main');
-				mainTag.appendChild(aboutPage);
-				addSpeakerCardWithData();
-			}
-			mobileMenu.parentNode.removeChild(mobileMenu);
-		});
-		document.querySelector('body').appendChild(mobileMenu);
-	}
+      const homePage = document.querySelector('.js-home-page');
+      if (homePage === null) {
+        const templateHomePage = document.querySelector('.js-template-home-page');
+        const aboutPage = templateHomePage.content.firstElementChild.cloneNode(true);
+        const mainTag = document.querySelector('main');
+        mainTag.appendChild(aboutPage);
+        window.addSpeakerCardWithData();
+      }
+      mobileMenu.parentNode.removeChild(mobileMenu);
+    });
+    document.querySelector('body').appendChild(mobileMenu);
+  }
 });
-
